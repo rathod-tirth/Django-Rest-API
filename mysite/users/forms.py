a40e7d81924fc8d536e8ef682942c61a82c08d6f
Tirth Rathod <122792938+rathod-tirth@users.noreply.github.com>
2024-10-23 19:34:45 +0530

    update add user form

--- mysite/users/forms.py
+++ mysite/users/forms.py
@@ -62,12 +62,19 @@ class AddUserForm(forms.ModelForm):
                     "required": "required",
                 }
             ),
-            # "date_of_birth": forms.DateInput(
-            #     attrs={
-            #         "class": "shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500",
-            #         "placeholder": "Bonnie",
-            #     }
-            # ),
+            "gender": forms.Select(
+                attrs={
+                    "class": "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500",
+                }
+            ),
+            "date_of_birth": forms.DateTimeInput(
+                attrs={
+                    "class": "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500",
+                    "placeholder": "Select date",
+                    "datepicker": "datepicker",
+                    "format": "yyyy-mm-dd",
+                }
+            ),
             "address": forms.Textarea(
                 attrs={
                     "class": "block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500",
